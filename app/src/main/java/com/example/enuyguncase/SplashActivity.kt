ea19e05919eddf4339e1fd4ff1aab64f77ac07ba
sazakli <azaklisalih61@gmail.com>
2025-08-07 20:44:13 +0300

    splash

---
+++ app/src/main/java/com/example/enuyguncase/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.enuyguncase
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.enuyguncase.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 2 saniye sonra MainActivity'ye geç
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 2 saniye
+    }
+} 
